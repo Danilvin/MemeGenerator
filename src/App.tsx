588439313229e14f1b5ef47a302b0de1ef952fd6
Danilvin <108340803+Danilvin@users.noreply.github.com>
2025-12-16 12:01:45 -0800

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import Homepage from "./pages/Home";
@@ -6,7 +6,6 @@ import EditPage from "./pages/Edit";
 
 function App() {
   return (
-    <BrowserRouter basename="/MemeGenerator/">
     <div className='App'>
       <h1 style={{marginLeft:"50px"}}>Meme Generator</h1>
       <Routes>
@@ -14,7 +13,6 @@ function App() {
         <Route path="/edit" element={<EditPage/>}/>
       </Routes>
     </div>
-    </BrowserRouter>
   );
 }
 
